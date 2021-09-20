945574404c1ec0885142bad63da8a96a63b0942e
dembicki <nikeid1235@gmail.com>
2021-09-20 16:39:18 +0200

    Project init

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,22 @@
+<script setup>
+import { ref } from 'vue'
+
+defineProps({
+  msg: String
+})
+
+const count = ref(0)
+</script>
+
+<template>
+  <h1>{{ msg }}</h1>
+
+  <p>Hello from vue.js wow, so fast omfg</p>
+  
+</template>
+
+<style scoped>
+a {
+  color: #42b983;
+}
+</style>
